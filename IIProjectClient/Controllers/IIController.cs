ac582f3b8060836947e6956035389ef57a63674a
filipklementsson <filipklementsson@hotmail.com>
2016-03-08 13:03:05 +0100

    Lagt till Controller ink Test view

---
+++ IIProjectClient/Controllers/IIController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using System.Web.Hosting;
+
+
+namespace IIProjectClient.Controllers
+{
+    public class IIController : Controller
+    {
+        // GET: II
+        XElement testXML;
+        string appDataFolder = HostingEnvironment.MapPath("/App_Data/");
+
+        public IIController()
+        {
+            testXML = XElement.Load(appDataFolder + "Test.xml");
+        }
+        public ActionResult Index()
+        {
+            return View(testXML);
+        }
+
+        public ActionResult Test()
+        {
+            return View(testXML);
+        }
+
+    }
+}
